db35caf32e1f77e7bafc066a4921d565bcb599e1
i-X-ce <iceci2804@gmail.com>
2026-07-09 16:11:23 +0900

    🔥 スペースを削除

--- display.py
+++ display.py
@@ -3,30 +3,23 @@ import json
 import time
 from screeninfo import get_monitors
 
+
 with open(
     "config.json",
     "r",
     encoding="utf-8"
 ) as f:
-
     config = json.load(f)
 
 
-
 def get_image_path(name):
-
     for image in config["images"]:
-
         if image["name"] == name:
-
             return image["file"]
 
     return None
 
-
-
 # 最初に1回だけウィンドウ作成
-
 def init_display():
 
     # ウィンドウ作成
@@ -48,15 +41,12 @@ def init_display():
 
     # 2枚目のモニターがあればそこへ移動
     if len(monitors) >= 2:
-
         second = monitors[1]
-
         cv2.moveWindow(
             "Stimulus",
             second.x,
             second.y
         )
-
     else:
         print("外部ディスプレイが見つかりません")
 
@@ -70,29 +60,20 @@ def init_display():
 
 
 def show_image(name, seconds):
-
-
     path = get_image_path(name)
-
-
     img = cv2.imread(path)
-
 
     cv2.imshow(
         "Stimulus",
         img
     )
 
-
     start = time.time()
 
-
     while time.time() - start < seconds:
-
         cv2.waitKey(1)
 
 
 
 def close_display():
-
     cv2.destroyAllWindows()
--- tobii.py
+++ tobii.py
@@ -7,147 +7,79 @@ import time
 tracker = None
 gaze_data_list = []
 
-def a():
-    print("sjskjsjk")
-    
 def connect():
-
-
     global tracker
-
-
     trackers = tr.find_all_eyetrackers()
-
-
     if len(trackers) == 0:
-
         print("Tobiiなし")
         return False
 
-
-
     tracker = trackers[0]
-
 
     print("接続成功")
     print(tracker.model)
 
-
     return True
-
-
-
 
 def gaze_callback(gaze_data):
 
-
     row = [
-
         time.time(),
-
 
         gaze_data["left_gaze_point_on_display_area"][0],
         gaze_data["left_gaze_point_on_display_area"][1],
 
-
         gaze_data["right_gaze_point_on_display_area"][0],
         gaze_data["right_gaze_point_on_display_area"][1],
 
-
         gaze_data["left_pupil_diameter"],
         gaze_data["right_pupil_diameter"]
-
     ]
-
 
     gaze_data_list.append(row)
 
-
-
-
 def start_recording():
-
-
     print("計測開始")
 
-
     tracker.subscribe_to(
-
         tr.EYETRACKER_GAZE_DATA,
-
         gaze_callback,
-
         as_dictionary=True
-
     )
-
-
-
-
 
 def stop_recording():
-
-
     print("計測終了")
 
-
     tracker.unsubscribe_from(
-
         tr.EYETRACKER_GAZE_DATA,
-
         gaze_callback
-
     )
-
-
 
     os.makedirs(
         "data",
         exist_ok=True
     )
 
-
-
     with open(
-
         "data/gaze_pupil_data.csv",
-
         "w",
-
         newline=""
-
     ) as f:
-
-
-
         writer = csv.writer(f)
-
-
-
         writer.writerow(
-
             [
-
             "Time",
-
             "LeftGazeX",
             "LeftGazeY",
-
             "RightGazeX",
             "RightGazeY",
-
             "LeftPupil",
             "RightPupil"
-
             ]
-
         )
-
 
         writer.writerows(
             gaze_data_list
         )
-
-
 
     print("保存完了")
